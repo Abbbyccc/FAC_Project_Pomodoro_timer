8842805906f05d92acd643f75d1f6a48d3b1cd7f
Abbbyccc <abbychien@hotmail.com>
2022-07-21 10:31:45 +0100

    rearrange files locatio

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-const timeDisplay = document.querySelector('.time-disply')
+const timeDisplaytest = document.querySelector('.time-disply')
 
 describe('check if time can display properly', () => {
     test('link time to the function', () => {
         start()
-        return equal(timeDisplay.innerHTML, '25:00')
+        return equal(timeDisplaytest.innerHTML, '25:00')
     })
 })
