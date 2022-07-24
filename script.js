ea89ae2a52fa79b36af4e23862ead80282787431
Abbbyccc <abbychien@hotmail.com>
2022-07-24 14:06:04 +0100

    add function to button

--- script.js
+++ script.js
@@ -1,37 +1,85 @@
-const timeDisplay = document.querySelector('.time-disply')
+const timeDisplay = document.querySelector('#time-display')
+const breakTimeDisplay = document.querySelector('#break-time-display')
+const pomodoroTimer = document.querySelector('#pomodoro-timer')
+const breakTimer = document.querySelector('#break-timer')
 
+const startBtn = document.querySelector('#start-btn')
 
 let timer = null
-let minutes = 25
-let seconds = 0
+pomodoroTimer.addEventListener('click', () => {
+    // pomodoroTimer.classList.add('pomodoro-clicked')
+    // breakTimer.classList.remove('break-clicked')
+    timeDisplay.classList.remove('hidden')
+    breakTimeDisplay.classList.add('hidden')
+    startBtn.onclick = pomodoroTimerCount()
+})
 
-function start() {
+
+breakTimer.addEventListener('click', () => {
+    // breakTimer.classList.add('break-clicked')
+    // pomodoroTimer.classList.remove('pomodoro-clicked')
+    timeDisplay.classList.add('hidden')
+    breakTimeDisplay.classList.remove('hidden')
+    startBtn.onclick = breakTimerCount()
+})
+
+
+function pomodoroTimerCount() {
+    let startMinutes = document.querySelector('#time-minutes').innerHTML
+    let minutes = startMinutes
+    let seconds = 0
     if (timer !== null) {
         return
     }
-    timer = setInterval(countdown, 1000)
+    timer = setInterval(() => {
+        if (seconds >= 0) {
+            seconds -= 1
+            if (minutes > 0 && seconds == -1) {
+                seconds = 59
+                minutes--
+            }
+            if (minutes == 0 && seconds == -1) {
+                const audio = new Audio('alarming.m4r')
+                audio.play()
+                return
+            }
+        } else if (minutes == 0 && seconds == -1) {
+            return
+        }
+        let m = minutes < 10 ? '0' + minutes : minutes
+        let s = seconds < 10 ? '0' + seconds : seconds
+        timeDisplay.innerHTML = `<span id="time-minutes">${m}</span>:${s}`
+    }, 1000)
 }
 
-function countdown() {
-    if (seconds >= 0) {
-        seconds -= 1
-        if (minutes > 0 && seconds == -1) {
-            seconds = 59
-            minutes--
-        }
-        if (minutes == 0 && seconds == -1) {
-            const audio = new Audio('alarming.m4r')
-            audio.play()
-            return
-
-        }
-    } else if (minutes == 0 && seconds == -1) {
+function breakTimerCount() {
+    let startMinutes = document.querySelector('#break-minutes').innerHTML
+    let minutes = startMinutes
+    let seconds = 0
+    if (timer !== null) {
         return
     }
-    let m = minutes < 10 ? '0' + minutes : minutes
-    let s = seconds < 10 ? '0' + seconds : seconds
-    timeDisplay.innerHTML = `${m}:${s}`
+    timer = setInterval(() => {
+        if (seconds >= 0) {
+            seconds -= 1
+            if (minutes > 0 && seconds == -1) {
+                seconds = 59
+                minutes--
+            }
+            if (minutes == 0 && seconds == -1) {
+                const audio = new Audio('alarming.m4r')
+                audio.play()
+                return
+            }
+        } else if (minutes == 0 && seconds == -1) {
+            return
+        }
+        let m = minutes < 10 ? '0' + minutes : minutes
+        let s = seconds < 10 ? '0' + seconds : seconds
+        breakTimeDisplay.innerHTML = `<span id="break-minutes">${m}</span>:${s}`
+    }, 1000)
 }
+
 
 
 function pause() {
@@ -45,5 +93,6 @@ function reset() {
     seconds = 0
     let m = minutes < 10 ? '0' + minutes : minutes
     let s = seconds < 10 ? '0' + seconds : seconds
-    timeDisplay.innerHTML = `${m}:${s}`
+    timeDisplay.innerHTML = `<span id="time-minutes">${m}</span>:${s}`
+    breakTimeDisplay.innerHTML = `<span id="break-minutes">${m}</span>:${s}`
 }
--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@ const timeDisplaytest = document.querySelector('.time-disply')
 
 describe('check if time can display properly', () => {
     test('link time to the function', () => {
-        start()
         return equal(timeDisplaytest.innerHTML, '25:00')
     })
 })
