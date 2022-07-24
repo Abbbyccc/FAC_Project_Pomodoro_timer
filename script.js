f8b927a50801776cda5980f8bacf691cb617a9a5
Abbbyccc <abbychien@hotmail.com>
2022-07-24 19:31:41 +0100

    add time setting function

--- script.js
+++ script.js
@@ -4,36 +4,64 @@ const pomodoroTimer = document.querySelector('#pomodoro-timer')
 const breakTimer = document.querySelector('#break-timer')
 
 const startBtn = document.querySelector('#start-btn')
-const pauseBtn = document.querySelector('#pause-btn')
 const resetBtn = document.querySelector('#reset-btn')
+
+const pomodoroSetting = document.querySelector('#pomodoro-setting')
+const breakSetting = document.querySelector('#break-setting')
+
+const pomodoroSettingform = document.querySelector('#time-form')
+const breakSettingform = document.querySelector('#break-time-form')
+
 
 let timer = null
 let minutes
 let seconds = 0
 
 pomodoroTimer.addEventListener('click', () => {
-    resetPomo()
+
     timeDisplay.classList.remove('hidden')
     breakTimeDisplay.classList.add('hidden')
-    const startMinutes = document.querySelector('#time-minutes').innerHTML
-    minutes = startMinutes
+
+    breakSettingform.classList.add('hidden')
+    pomodoroSettingform.classList.remove('hidden')
+
+    let startMinutes = document.querySelector('#time-minutes')
+    pomodoroSetting.addEventListener('click', (event) => {
+        event.preventDefault()
+        settingTime = document.forms["time-form"].timers;
+        minutes = Number(settingTime.value)
+        startMinutes.innerHTML = minutes
+        seconds = 0
+    })
+
     startBtn.setAttribute('onclick', 'pomodoro()')
     resetBtn.setAttribute('onclick', 'resetPomo()')
 })
 
 breakTimer.addEventListener('click', () => {
-    resetbreak()
     timeDisplay.classList.add('hidden')
     breakTimeDisplay.classList.remove('hidden')
+
+    breakSettingform.classList.remove('hidden')
+    pomodoroSettingform.classList.add('hidden')
+
     pomodoroTimer.classList.remove('pomodoro-clicked')
     breakTimer.classList.add('break-clicked')
-    const breakMinutes = document.querySelector('#break-minutes').innerHTML
-    minutes = breakMinutes
+    const breakMinutes = document.querySelector('#break-minutes')
+    breakSetting.addEventListener('click', (event) => {
+        event.preventDefault()
+        settingTime = document.forms["break-time-form"]['break-timer'];
+        minutes = Number(settingTime.value)
+        breakMinutes.innerHTML = minutes
+        seconds = 0
+    })
+
     startBtn.setAttribute('onclick', 'breakTime()')
     resetBtn.setAttribute('onclick', 'resetbreak()')
 })
 
 pomodoroTimer.click()
+
 function pomodoro() {
     if (timer !== null) {
         return
@@ -46,7 +74,9 @@ function pomodoro() {
                 minutes--
             }
             if (minutes == 0 && seconds == -1) {
-                alarmActivate()
+                const audio = new Audio('alarming.m4r')
+                audio.play()
+                setTimeout(function () { alert("Good Job! Have some rest"); }, 500);
                 breakTimer.click()
                 return
             }
@@ -71,7 +101,9 @@ function breakTime() {
                 minutes--
             }
             if (minutes == 0 && seconds == -1) {
-                alarmActivate()
+                const audio = new Audio('alarming.m4r')
+                audio.play()
+                setTimeout(function () { alert("Let's go back to work!"); }, 500);
                 pomodoroTimer.click()
                 return
             }
@@ -84,12 +116,10 @@ function breakTime() {
     }, 1000)
 }
 
-
 function pause() {
     clearInterval(timer)
     timer = null
 }
-
 
 function resetPomo() {
     clearInterval(timer)
@@ -99,7 +129,7 @@ function resetPomo() {
     let s = seconds < 10 ? '0' + seconds : seconds
     timeDisplay.innerHTML = `<span id="time-minutes">${m}</span>:${s}`
     timer = null
-
+    pomodoroTimer.click()
 }
 
 function resetbreak() {
@@ -110,10 +140,6 @@ function resetbreak() {
     let s = seconds < 10 ? '0' + seconds : seconds
     breakTimeDisplay.innerHTML = `<span id="break-minutes">${m}</span>:${s}`
     timer = null
+    breakTimer.click()
 }
 
-function alarmActivate() {
-    const audio = new Audio('alarming.m4r')
-    audio.play()
-    setTimeout(function () { alert("Extraction is finished"); }, 500);
-}
